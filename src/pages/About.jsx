d73067cf4d9d3187bc07282594cd779f4ccfdb81
dextboy <dextboy@outlook.com>
2023-05-26 13:28:06 +0800

    added pages/about file

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <div style={{ fontSize: "24px" }}>
+        This website is a study companion web application by Tan Dexter and Joy
+        Liu.
+      </div>
+      <a href="mailto:tandexter98@gmail.com?cc=liuzehui03@gmail.com&subject=Issue(s)%20with%20StudyPal">
+        Send email for issues
+      </a>
+    </>
+  );
+};
+
+export default About;
