0a7aaa7e44d684ee72172cb051f49687bfbbfa14
liuzehui03 <liuzehui03@gmail.com>
2023-06-24 19:40:43 +0800

    added Pomodoro

---
+++ src/components/Pomodoro.jsx
@@ -0,0 +1,30 @@
+
+import Timer from "./Timer";
+import Settings from "./Settings";
+import {useState} from "react";
+import SettingsContext from "./SettingsContext";
+import './style/Pomodoro.css';
+
+function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
+  return (
+    <main>
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
+    </main>
+  );
+}
+
+export default App;
