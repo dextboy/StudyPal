3dc9c362e1d6646417b9e724ace7811d60787c08
dextboy <dextboy@outlook.com>
2023-07-13 15:11:32 +0800

    Hooked calendar up to backend

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,46 +1,87 @@
-import React from "react";
-import Fullcalendar from "@fullcalendar/react";
+import React, { useEffect, useState } from "react";
+import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
-import { useState } from "react";
-import { v4 as uuid } from "uuid";
+import { useAuth } from "../context/AuthProvider";
+import { supabase } from "../supabase/client";
 
-function Calendar() {
+const Calendar = () => {
   const [events, setEvents] = useState([]);
-  const handleSelect = (info) => {
+  const { user } = useAuth();
+
+  const handleSelect = async (info) => {
     const { start, end } = info;
-    const eventNamePrompt = prompt("Enter, event name");
+    const eventNamePrompt = prompt("Enter event name");
+
     if (eventNamePrompt) {
-      setEvents([
-        ...events,
+      const { data, error } = await supabase.from("events").insert([
         {
-          start,
-          end,
+          start: start.toISOString(),
+          end: end.toISOString(),
           title: eventNamePrompt,
-          id: uuid(),
+          user_id: user?.id, // Replace with the actual user ID
         },
       ]);
+
+      if (error) {
+        console.log("Error saving event:", error.message);
+      } else {
+        setEvents([
+          ...events,
+          {
+            start: start.toISOString(),
+            end: end.toISOString(),
+            title: eventNamePrompt,
+            user_id: user?.id,
+          },
+        ]);
+      }
     }
   };
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const { data, error } = await supabase
+        .from("events")
+        .select("*")
+        .eq("user_id", user?.id); // Replace with the actual user ID
+
+      if (data) {
+        const formattedEvents = data.map((event) => ({
+          ...event,
+          start: new Date(event.start),
+          end: new Date(event.end),
+        }));
+        setEvents(formattedEvents);
+      }
+
+      if (error) {
+        console.log("Error fetching events:", error.message);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
   return (
     <div>
-      <Fullcalendar
+      <FullCalendar
         editable
         selectable
         events={events}
         select={handleSelect}
-        headerToolbar={{
-          start: "today prev,next", // will normally be on the left. if RTL, will be on the right
-          center: "title",
-          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
-        }}
         plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-        initialView={"dayGridMonth"}
-        height={"100vh"}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          start: "today prev,next",
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        height="100vh"
       />
     </div>
   );
-}
+};
 
 export default Calendar;
