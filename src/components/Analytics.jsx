a933975b7571c63b18bdc03de03aa59c743925b2
dextboy <dextboy@outlook.com>
2023-07-13 13:17:23 +0800

    Added analytics page

---
+++ src/components/Analytics.jsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabase/client";
+import { useAuth } from "../context/AuthProvider";
+
+const Analytics = () => {
+  const { user } = useAuth();
+  const [startTime, setStartTime] = useState(new Date());
+  const [totalDays, setTotalDays] = useState(0);
+  const [totalHours, setTotalHours] = useState(0);
+  const [totalMinutes, setTotalMinutes] = useState(0);
+
+  const startSession = () => {
+    setStartTime(new Date());
+  };
+
+  useEffect(() => {
+    startSession();
+
+    const handleBeforeUnload = (event) => {
+      event.preventDefault();
+      event.returnValue = ""; // This is required for browser compatibility
+      endSession();
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      endSession(); // Call endSession function before unmounting the component
+    };
+  }, []);
+
+  const calculateDuration = (endTime, startTime) => {
+    const sessionDuration = Math.floor((endTime - startTime) / 1000); // Duration in seconds
+    const totalDurationSeconds =
+      totalDays * 86400 +
+      totalHours * 3600 +
+      totalMinutes * 60 +
+      sessionDuration;
+    const totalDurationDays = Math.floor(totalDurationSeconds / 86400); // Number of days
+    const totalDurationHours = Math.floor(
+      (totalDurationSeconds % 86400) / 3600
+    ); // Remaining hours
+    const totalDurationMinutes = Math.floor((totalDurationSeconds % 3600) / 60); // Remaining minutes
+
+    setTotalDays(totalDurationDays);
+    setTotalHours(totalDurationHours);
+    setTotalMinutes(totalDurationMinutes);
+  };
+
+  const endSession = async () => {
+    const endTime = new Date();
+    calculateDuration(endTime, startTime);
+
+    // Insert a new session row with the session duration
+    try {
+      await supabase.from("user_sessions").insert([
+        {
+          user_id: user?.id,
+          duration: Math.floor((endTime - startTime) / 1000),
+        },
+      ]);
+    } catch (error) {
+      console.log("Error creating session:", error);
+    }
+
+    setStartTime(new Date());
+    setTotalDays(0);
+    setTotalHours(0);
+    setTotalMinutes(0);
+  };
+
+  useEffect(() => {
+    const fetchSessionDetails = async () => {
+      const { data, error } = await supabase
+        .from("user_sessions")
+        .select("duration")
+        .eq("user_id", user?.id);
+
+      if (data) {
+        // Calculate the total duration by summing up all the durations
+        const totalDurationSeconds = data.reduce(
+          (sum, row) => sum + row.duration,
+          0
+        );
+        const totalDurationDays = Math.floor(totalDurationSeconds / 86400); // Number of days
+        const totalDurationHours = Math.floor(
+          (totalDurationSeconds % 86400) / 3600
+        ); // Remaining hours
+        const totalDurationMinutes = Math.floor(
+          (totalDurationSeconds % 3600) / 60
+        ); // Remaining minutes
+
+        setTotalDays(totalDurationDays);
+        setTotalHours(totalDurationHours);
+        setTotalMinutes(totalDurationMinutes);
+      }
+
+      if (error) {
+        console.log("Error fetching session details:", error);
+      }
+    };
+
+    fetchSessionDetails();
+  }, [user?.id]);
+
+  return (
+    <div>
+      <h1>
+        Total Time on app: {totalDays} days {totalHours} hours {totalMinutes}{" "}
+        minutes
+      </h1>
+    </div>
+  );
+};
+
+export default Analytics;
